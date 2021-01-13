0a287234d985b53391786c1760639b8c0b1b705b
Michael Michot <michael.michot@pharmasimple.com>
2021-01-13 16:37:18 +0100

    [FIX] following Raph review + tests simplification

--- odoo/odoo/addons/phs_stock/tests/test_stock_picking_batch.py
+++ odoo/odoo/addons/phs_stock/tests/test_stock_picking_batch.py
@@ -6,45 +6,12 @@ class TestStockPickingBatchRule(TransactionCase):
 
     def setUp(self):
         super(TestStockPickingBatchRule, self).setUp()
-        self.so = self.env['sale.order']
         self.picking = self.env['stock.picking']
         self.picking_batch = self.env['stock.picking.batch']
         self.rules = self.env['stock.picking.batch.rule']
         # clean existings rules
         self.rules.search([]).unlink()
-        # validate all existing pickings
-        pickings_to_validate = self.env['stock.picking'].search([('state', '=', 'assigned')])
-        pickings_to_validate.with_context(cancel_backorder=True)._action_done()
-        # update stock qty to force availability for future pickings
-        self.partner_id = self.env['res.partner'].create({'name': 'Wood Corner Partner'})
-        self.product_id_1 = self.env['product.product'].create({'name': 'Large Desk'})
-        self.product_id_2 = self.env['product.product'].create({'name': 'Conference Chair'})
-        self.po_vals = {
-            'partner_id': self.partner_id.id,
-            'order_line': [
-                (0, 0, {
-                    'name': self.product_id_1.name,
-                    'product_id': self.product_id_1.id,
-                    'product_qty': 5.0,
-                    'product_uom': self.product_id_1.uom_po_id.id,
-                    'price_unit': 500.0,
-                })],
-        }
-        po = self.env['purchase.order'].create(self.po_vals)
-
-        po.order_line.product_qty = 1000
-        po.button_confirm()
-
-        first_picking = po.picking_ids
-        first_picking.move_lines.quantity_done = 1000
-        first_picking.button_validate()
-        # create orders
-        for i in range(0,50):
-            so = self.so.create({'partner_id': self.env.ref("base.res_partner_12").id,
-                            'order_line': [(0, 0, {'product_id': self.product_id_1.id,
-                                                'product_uom_qty': 1,
-                                                'price_unit': 5})]})
-            so.action_confirm()
+        self.type_pick_out_id =	self.env.ref("stock.picking_type_out")
 
 
     def test_no_rule_no_batch(self):
@@ -53,20 +20,20 @@ class TestStockPickingBatchRule(TransactionCase):
         self.assertEqual(len(batch), 0)
 
     def test_rule_matching_all_pickings(self):
-        """One rule matching all picking (50) split in batch of 3 orders
-            expected result: 8 batch
+        """One rule matching all picking (8) split in batch of 2 orders
+            expected result: 4 batch
         """
         filter_all = self.env['ir.filters'].create({'name': 'All',
                                                     'model_id': 'stock.picking',
-                                                    'domain': '[]',
+                                                    'domain': "[]",
                                                     'context': "{'group_by': []}",
                                                     'sort': '[]'})
         rule = self.rules.create({'name': 'All',
                             'filter_id': filter_all.id,
                             'nbr_box': 2,
-                            'nbr_order': 3,
-                            'picking_type_id': self.env['stock.picking.type'].search([('name', '=', 'Pack')])[0].id,
+                            'nbr_order': 1,
+                            'picking_type_id': self.type_pick_out_id.id,
                             })
         batch = rule.batch_creation()
-        self.assertEqual(len(batch), 8)
+        self.assertEqual(len(batch), 4)
     
