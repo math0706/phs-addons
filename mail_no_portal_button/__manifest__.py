059313db569079f270f8a692fed8772323fd3239
Michael Michot <michael.michot@pharmasimple.com>
2021-04-27 17:42:13 +0200

    [ADD] module mail_no_portal_button

---
+++ mail_no_portal_button/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Pharmasimple (https://www.pharmasimple.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Notification - Disable portal button",
+    "category": "Custom",
+    "summary": "Disable portal button on email notification  ",
+    "version": "14.0.1.0.0",
+    "author": "Pharmasimple",
+    "license": "AGPL-3",
+    "website": "https://github.com/akretion/phs-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/mail_data.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
