91cce2a15ac5c93f5eab657991fc477e54723ba2
Michael Michot <michael.michot@pharmasimple.com>
2021-06-07 19:21:31 +0200

    [ADD] New module to allow picking based on multi_barcode

---
+++ shopfloor_mobile_multi_barcode/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Pharmasimple (https://www.pharmasimple.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Shopfloor multi barcode",
+    "category": "Warehouse Management",
+    "summary": "Batch picking with multi barcode on products",
+    "version": "14.0.1.0.0",
+    "author": "Pharmasimple",
+    "license": "AGPL-3",
+    "website": "https://github.com/akretion/phs-addons",
+    "depends": [
+        "shopfloor_mobile",
+        "product_multi_barcode",
+    ],
+    "data": ["templates/assets.xml"],
+    "installable": True,
+    "application": False,
+}
