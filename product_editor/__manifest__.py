2295890970631147fe5bca86872fc501c46f43e4
mathieu <mathieu@pharmasimple.com>
2021-06-08 08:47:19 +0200

    [ADD] security on product_product and product_template

---
+++ product_editor/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Pharmasimple (https://www.pharmasimple.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Group Management",
+    "category": "Custom",
+    "summary": "Group Management",
+    "version": "14.0.1.0.0",
+    "author": "Pharmasimple",
+    "license": "AGPL-3",
+    "website": "https://github.com/akretion/phs-addons",
+    "depends": ["product"],
+    "data": [
+        'security/product_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    "installable": True,
+}
