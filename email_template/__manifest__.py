090caee1e007d0387eee53d447040ea8c7ae69bf
mathieu <mathieu@pharmasimple.com>
2021-11-03 11:30:15 +0100

    [FIX] template mail notification

---
+++ email_template/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Pharmasimple (https://www.pharmasimple.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Email template",
+    "category": "Custom",
+    "summary": "",
+    "version": "14.0.1.0.0",
+    "author": "Pharmasimple",
+    "license": "AGPL-3",
+    "website": "https://www.pharmasimple.com",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "views/mail_notification_paynow_online_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
