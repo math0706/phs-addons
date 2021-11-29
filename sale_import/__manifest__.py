314ca461587143519f3f1e8f9db18b2e7e59b12e
Michael Michot <michael.michot@pharmasimple.com>
2021-11-29 11:31:22 +0100

    [ADD] module sale_import

---
+++ sale_import/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "sale import",
+    "version": "14.0.1.0.1",
+    "category": "Sale",
+    "author": "BHC",
+    "website": "https://github.com/akretion/phs-addons",
+    "license": "AGPL-3",
+    "depends": ["base", "sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/import_sale_order.xml",
+    ],
+    "installable": True,
+}
