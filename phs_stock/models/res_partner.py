d1cb80de5b352d1cc9d6e8ac069de825f8a7baa0
mathieu <mathieu@pharmasimple.com>
2022-01-11 16:47:02 +0100

    [IMP] update button on res partner for stock warehouse orderpoint

--- phs_stock/models/res_partner.py
+++ phs_stock/models/res_partner.py
@@ -9,12 +9,40 @@ class ResPartner(models.Model):
 
     def _compute_stock_orderpoint(self):
         for partner in self:
-            stock = self.env["stock.warehouse.orderpoint"]
-            stock = stock.search(
-                [("supplier_id.id", "=", self.id), ("qty_to_order", ">", 0)]
-            )
-            partner.stock_orderpoint = len(stock)
+            orderpoint = self.domain_stock_orderpoint(True, partner)
+            partner.stock_orderpoint_count = len(orderpoint)
 
-    stock_orderpoint = fields.Integer(
+    stock_orderpoint_count = fields.Integer(
         compute="_compute_stock_orderpoint", string="Stock Orderpoint"
     )
+
+    def domain_stock_orderpoint(self, qty_to_order, partner_id):
+        oderpoint = self.env["stock.warehouse.orderpoint"]
+        product_supplier_info = self.env["product.supplierinfo"]
+        product_supplier_info = product_supplier_info.search(
+            [("name.id", "=", partner_id.id)]
+        )
+        domain = [
+            (
+                "product_tmpl_id.id",
+                "in",
+                product_supplier_info.mapped("product_tmpl_id.id"),
+            )
+        ]
+        if qty_to_order:
+            domain.append(("qty_to_order", ">", 0))
+        orderpoint = oderpoint.search(domain)
+        return orderpoint
+
+    def stock_orderpoint(self):
+        orderpoint = self.domain_stock_orderpoint(False, self)
+
+        if orderpoint:
+            return {
+                "view_type": "tree",
+                "view_mode": "tree",
+                "res_model": "stock.warehouse.orderpoint",
+                "domain": [("product_id", "in", orderpoint.mapped("product_id").ids)],
+                "context": {"search_default_filter_to_reorder": 1},
+                "type": "ir.actions.act_window",
+            }
